63caf76f6fa123bc7d7932d085fad96db524c926
maackii <sabine.kokot@gmail.com>
2017-01-06 12:04:45 +0100

    Add files via upload

--- client.c
+++ client.c
@@ -6,24 +6,18 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-/*Bitflags*/
-#define hasTail 1
-
 void awesomeError(const char *msg)
 {
     perror(msg);
 }
 
 int main(int argc, char* argv[]) {
-
     struct sockaddr_in servAddr;
     struct hostent * server;
 
     int mySockFile, portNumbr;
     intmax_t cnt;
-
-    char buffer [128];
-    uint8_t* myfirstpackage;
+    char buffer[100];
 
     if (argc < 3) {
         fprintf(stderr,"ERROR, no port provided\n");
@@ -37,13 +31,14 @@ int main(int argc, char* argv[]) {
         exit(0);
     }
 
-    /*Create socket*/
+    /*I socket my socket*/
     mySockFile = socket(AF_INET, SOCK_STREAM, 0);
     if (mySockFile < 0){
         awesomeError("Socket Error!");
     }
 
     bzero ( (char *) &servAddr, sizeof(servAddr));
+
     servAddr.sin_family = AF_INET;
     bcopy((char *)server->h_addr,
           (char *)&servAddr.sin_addr.s_addr,
@@ -51,32 +46,19 @@ int main(int argc, char* argv[]) {
     servAddr.sin_port = htons(portNumbr);
     if (connect(mySockFile,(struct sockaddr *) &servAddr,sizeof(servAddr)) < 0)
         awesomeError("ERROR connecting");
-    /*Sending*/
-    printf("Please enter a message.\n");
-    bzero(buffer,128);
-    fgets(buffer,127,stdin);
 
-    if (strlen(buffer)>1){
-      myfirstpackage =  malloc(strlen(buffer)+2);
-      myfirstpackage[0] = (uint8_t) hasTail;
-      myfirstpackage[1] = (uint8_t) strlen(buffer);
-      memcpy( &(myfirstpackage[2]), buffer,strlen(buffer));
-  } else {
-    myfirstpackage = malloc(1);
-    myfirstpackage[0]=0;
-  }
-  cnt = write(mySockFile,myfirstpackage,strlen(buffer)+1);
-  if (cnt < 0)
-      awesomeError("ERROR writing to socket");
+    printf("Please enter the message: ");
 
-  bzero(buffer,128);
-  cnt = read(mySockFile,buffer,127);
-  if (cnt < 0){
-      awesomeError("ERROR reading from socket");
-  }
-  printf("%s\n",buffer);
-  free(myfirstpackage);
-  close(mySockFile);
-
+    bzero(buffer,100);
+    fgets(buffer,99,stdin);
+    cnt = write(mySockFile,buffer,strlen(buffer));
+    if (cnt < 0)
+        awesomeError("ERROR writing to socket");
+    bzero(buffer,100);
+    cnt = read(mySockFile,buffer,99);
+    if (cnt < 0)
+        awesomeError("ERROR reading from socket");
+    printf("%s\n",buffer);
+    close(mySockFile);
     return 0;
 }
