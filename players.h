47cbde3f2432b695733c5dfb4d0c6bfde195b17a
maackii <sabine.kokot@gmail.com>
2017-02-17 13:11:43 +0100

    Added Update Functions (Points&Handcards)

--- players.h
+++ players.h
@@ -1,12 +1,10 @@
 #ifndef PLAYERS_H_INCLUDED
 #define PLAYERS_H_INCLUDED
 
-
+#include "cardpiles.h"
 #define MAXPLAYERS 3
 //#define MAXROUNDS 100
 #define MAXHANDCARDS 2
-
-
 
 #define ERROR -1
 #define SUCCESS 1
@@ -19,9 +17,9 @@ enum role{
 
 typedef struct player{
     int socketID;
+    int status;
     int role;
     int handCards;
-    int requestedCards;
     int points;
     char* name;
     char** cardText;                /** [0] = question **/
@@ -34,6 +32,8 @@ typedef struct player{
 player_t* createPlayer(player_t* head, int socket);
 void printNodes(player_t* head);    /******remove after testing*/
 void destroyPlayers(player_t** head);
+int updateHandcards(player_t** head, pile_t** draw, pile_t** discard);
+int updatePoints(player_t** head, int winnerID);
 
 
 #endif
