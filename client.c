e36115a5948b663a98941fe14726befe917a109e
Rhumo <speaking.muffin@web.de>
2017-02-18 20:39:35 +0100

    alles

--- client.c
+++ client.c
@@ -7,6 +7,10 @@
 #include <netdb.h>
 #include "players.h"
 #include "connectivity.h"
+#include "cardpiles.h"
+#include "cardpiles.c"              // ??????????ß      warum .c statt .h    ????????????
+
+#define HANDCARDS_MAX 5
 
 /*linked list for answers
 struct answerCollect{
@@ -15,61 +19,171 @@ struct answerCollect{
 };
 */
 
-void update_status(player_t player){
+// wählt antworten aus handcards, löscht diese nach auswahl aus handcards und fügt sie zu replies hinzu
+void choose_replies(player_t *player, int gaps){
+    int i, scan, check_input = 1;
 
-    uint8_t typeFlag = -1, typeID = -1;
+    printf("GAPS: %d\n", gaps);
+
+    gimme_good_lines("", __LINE__);
+    if(player->replies == NULL){
+        player->replies = malloc(gaps * sizeof(char*));
+        for (i = 0; i < gaps; i++){
+            player->replies[i] = NULL;
+            gimme_good_lines("", __LINE__);
+        }
+    }
+
+    for (i = 0; i < gaps; i++){
+
+        gimme_good_lines("", __LINE__);
+
+        do {
+            printf("Please choose answer for %dst gap. [type 1-5]\n", i + 1);
+            scanf("%d", &scan);
+            check_input = scan;
+
+            if(check_input < 1 || check_input > 5){
+                printf("Wrong input. Please choose number between 1 and %d.\n", HANDCARDS_MAX);
+            }else if(player->cardText[check_input-1] == NULL){
+                printf("Wrong input, answer already used.\n");
+            }
+
+        }while(check_input < 1 || check_input > 5 || player->cardText[check_input -1] == NULL);
+
+        gimme_good_lines("", __LINE__);
+        player->replies[i] = malloc(strlen(player->cardText[scan -1]) * sizeof(char));
+        strcpy(player->replies[i], player->cardText[scan -1]);
+        player->cardText[scan -1] = NULL;
+
+    }
+    gimme_good_lines("", __LINE__);
+    for (i = 0; i < gaps; i++){
+        printf("reply %d: %s\n", i, player->replies[i]);
+    }
+}
+
+void display_cards(player_t player, gameState_t game){
+    int i;
+
+    printCard(game.question, 1);
+
+    for(i = 0; player.role != CARDCZAR && i < HANDCARDS_MAX; i++){
+        if(player.cardText[i] != NULL)
+            printCard(player.cardText[i], 0);
+    }
+
+}
+
+uint8_t ok_cardnumber(player_t *player){
+
+    if(player->handCards == HANDCARDS_MAX){
+        return C_TYPE_OK;
+    }
+    else return 0;
+
+}
+
+void update_status(player_t *player, gameState_t *game){
+
+    uint8_t typeFlag = -1, typeID = -1, getInt = 111;
     int numb_messg = 0, i = 0, j = 0;
-    char* recMessages[] = {"bli", "bla", "blubb", "blopp", "bleh"};
+    char** recMessages/*[] = {"bli", "bla", "blubb", "blopp", "bleh"}*/;
 
-    printf("received data type message(s):\n");
-    typeFlag = getStatus(player.socketID);
-    //numb_messg = getDataPackage(player.socketID, &recMessages, &typeID);
+
+    gimme_good_lines("", __LINE__);
+
+    printf("received data type message(s)!\n");
+    typeFlag = getStatus(player->socketID);
+
+    //printf("typeFlag is: %d\n", typeFlag);        //testing
+
     /*for (int i = 0; i < cnt; i++){
         printf("%s\n", recMessages[i]);
     }*/
-    typeID = D_TYPE_HANDCARDS;
+    //typeID = D_TYPE_HANDCARDS;
 
+    gimme_good_lines("", __LINE__);
 
-    switch (typeID){
+    switch (typeFlag){
+
         case D_TYPE_HANDCARDS:
 
-            for(i = 0; i < numb_messg; i++){
-                for(j = 0; j < 5; j++){
+            numb_messg = getDataPackage(player->socketID, &recMessages, &typeID);
+            for(i = 0, j = 0; i < numb_messg && j < 5; j++){
 
-                    if(player.cardText[j] == NULL){
+                //printf("i = %d, j = %d\n", i, j); //Only testing
 
-                        player.cardText[j] = malloc(strlen(recMessages[i]) * sizeof(char));
-                        strcpy(player.cardText[j], recMessages[i]);
+                gimme_good_lines("", __LINE__);
 
-                    }
+                if(player->cardText[j] == NULL){
 
+                    gimme_good_lines("", __LINE__);
+
+                    player->cardText[j] = malloc(strlen(recMessages[i]) * sizeof(char));
+                    strcpy(player->cardText[j], recMessages[i]);
+
+                    (player->handCards)++;
+                    i++;
                 }
             }
 
-            for (int i = 0; i < 5; i++){
-                printf("%s\n", player.cardText[i]);
-            }
+            /*for(i = 0; i < 5; i++){
+                if(player->cardText[i] != NULL)
+                    printCard(player->cardText[i], 0);
+            }*/
+            printf("number of handcards: %d\n", player->handCards);
 
-            break;
-
-        case D_TYPE_NAME:
-
-            break;
-
-        case D_TYPE_POINTS:
 
             break;
 
         case D_TYPE_QUESTION:
 
+            numb_messg = getDataPackage(player->socketID, &recMessages, &typeID);
+            gimme_good_lines("", __LINE__);
+            game->question = malloc(strlen(recMessages[0]) * sizeof(char));
+            strcpy(game->question, recMessages[0]);
+
+            printf("[%d gaps] question: %s\n", typeID, game->question);
+
             break;
 
+        case D_TYPE_NAME:
+
+            numb_messg = getDataPackage(player->socketID, &recMessages, &typeID);
+            gimme_good_lines("", __LINE__);
+            player->name = malloc(strlen(recMessages[0]) * sizeof(char));
+            strcpy(player->name, recMessages[0]);
+            printf("Ur [ID: %d] name is '%s'\n", typeID, player->name);
+
+            break;
+
+        case D_TYPE_POINTS:
+
+            getIntPackage(player->socketID, &getInt);
+
+            gimme_good_lines("", __LINE__);
+            player->points = getInt;
+            printf("U have %d points!\n", player->points);
+
+            break;
+
+
         case D_TYPE_ROLE:
+
+            getIntPackage(player->socketID, &getInt);
+
+
+            gimme_good_lines("", __LINE__);
+            player->role = getInt;
+            printf("U are role %d\n", player->role);
 
             break;
 
         default:
-            printf("no valid typeID");
+
+            gimme_good_lines("", __LINE__);
+            printf("no valid typeID\n");
             break;
     }
 
@@ -78,8 +192,10 @@ void update_status(player_t player){
 
 int main(int argc, char* argv[]) {
 
+
+//-------------------connect client start---------------------------
     struct hostent * server;
-    int mySocket, portNumbr, check;
+    int mySocket, portNumbr, check, i;
     intmax_t cnt;
     char buffer [128];
     char* messages[2] = {"These are", "test messages"};
@@ -108,9 +224,10 @@ int main(int argc, char* argv[]) {
     }
     printf("I am connected\n");
 
+//-------------------connect client end---------------------------
 
-
-    if (MSG_DATA == getStatus(player.socketID)){
+    //---test---test---test---test---test---test---test---test---test---test----
+    /*if (MSG_DATA == getStatus(player.socketID)){
       typeFlag = getStatus(player.socketID);
       printf("I am type Flag: %d\n", typeFlag);
       cnt = getDataPackage(player.socketID, &recMessages, &typeID);
@@ -119,7 +236,7 @@ int main(int argc, char* argv[]) {
           printf("%s\n", recMessages[i]);
         }
 
-      }
+      }*/
 
 
     /*type = getStatus(player.socketID);
@@ -129,27 +246,42 @@ int main(int argc, char* argv[]) {
       }
     }*/
 
+    //---test---test---test---test---test---test---test---test---test---test----
 
-    //----------------------------STATE MACHINE START--------------------------------
+
+//----------------------------STATE MACHINE START--------------------------------
 
     //awesome variables
     int data_type = -1, break_loop = 0;
     uint8_t  ctrl = 111;
+    gameState_t game;
+
+    //initialize Player.cards[x] with NULL
+    player.cardText = malloc( 5 * sizeof(char*));
+    for(i = 0; i < 5; i++){
+        player.cardText[i] = NULL;
+    }
+
 
     //While loop
-    while(break_loop < 20) {
+    while(break_loop < 5) {
 
         // Status Update
+        ctrl = 111;
         data_type = getStatus(player.socketID);
 
-        data_type = MSG_DATA;  //just for testing
+        //data_type = MSG_DATA;  //just for testing
 
 
         if(MSG_CTRL == data_type) {
+
+            gimme_good_lines("is ctrl message\n", __LINE__);
             getIntPackage(player.socketID, &ctrl);
 
         }else if(MSG_DATA == data_type){
-            update_status(player);
+
+            gimme_good_lines("", __LINE__);
+            update_status(&player, &game);
 
         }else{
             printf("no new ctrl- or data-messages\n");
@@ -157,15 +289,13 @@ int main(int argc, char* argv[]) {
 
 
 
+        break_loop++;
         //Switch statement
         switch(ctrl) {
 
-
             // Display new Round / New Game prompt / display winner & game status
-            case 0:
+            case C_TYPE_NEW_ROUND:
                             //Check if first round
-
-                            //get hand cards & question from server
 
                             //Check number of cards
 
@@ -174,18 +304,21 @@ int main(int argc, char* argv[]) {
 
 
             //Choose & send funny answers
-            case 2:
+            case C_TYPE_DISPLAY_CARDS:
                             //display answers and / or question
-
+                display_cards(player, game);
                             //choose funny answer(s)
-
+                gimme_good_lines("", __LINE__);
+                choose_replies(&player, gaps(game.question));
+                gimme_good_lines("", __LINE__);
                             //send funny answer(s)
+                sendDataPackage(player.socketID, D_TYPE_HANDCARDS, (uint8_t ) player.socketID, gaps(game.question), player.replies);
+
                 break;
 
 
             //Czar choose winner
-            case 4:
-
+            case C_TYPE_DISPLAY_ANSWERS:
                             //get all funny answers
 
                             //display all funny answers to all (shuffled)
@@ -195,17 +328,18 @@ int main(int argc, char* argv[]) {
                             //Czar sends winner to server
                 break;
 
-
-            default:
-                printf("non valid status! ...restart switching...\n");
-                break_loop++;
+            case C_TYPE_OK:     //sends 1 if number of cards ok, sends 0 if not
+                sendIntPackage(player.socketID, MSG_CTRL, ok_cardnumber(&player));
                 break;
 
+            default:
+                printf("no valid / new status! ...restart switching...\n");
+                break;
 
         }
     }
 
-    //----------------------------STATE MACHINE END--------------------------------
+//----------------------------STATE MACHINE END--------------------------------
 
 
 
